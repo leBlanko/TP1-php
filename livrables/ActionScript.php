d8c8d346169b0e78063b3ba821dd8d53761c5966
Jackkson <simon.riquart@icloud.com>
2017-01-12 02:25:27 +0100

    Create ActionScript.php
    La meilleure classe du monde, celle qu'il faudra m'expliquer en priorité vu que c'est la main et que je sèche complètement dessus

---
+++ livrables/ActionScript.php
@@ -0,0 +1,32 @@
+<?php
+class ActionScript extends File
+{
+	private $currentFile;
+
+	public function select(argv[0])
+	{
+		$this -> Entity.getKey();
+		for($i = 0; $i < key[].length; $i++)
+		{
+			///lolololololol
+		}
+	}
+
+	public function put()
+	{
+		/////mddrrrrrrrrrrrrrrr
+	}
+
+	public function update()
+	{
+		///zzzzzzzzzzzzzzz
+	}
+
+	public function delete()
+	{
+		///// Donald Trump pushing the red button #BOUM
+	}
+
+
+}
+?>
